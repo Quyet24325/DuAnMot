266df26f40d1af8a7e456de8939febe00e7edb29
Quyet24325 <quyet24325@gmail.com>
2024-11-13 13:45:23 +0700

    feat: db

---
+++ connect/connect.php
@@ -0,0 +1,20 @@
+<?php 
+
+class connect{
+    public function connect(){
+        $serverName = 'localhost';
+        $userName= 'root';
+        $password = '';
+        $db ='duanmot';
+        try {
+            $conn = new PDO("mysql:host=$serverName;dbname=$db",username:$userName,password:$password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (\Throwable $th) {
+           echo "Kết nối thất bại". $th->getMessage();
+           return null;
+        }
+    }
+}
+
+?>
