a60f515b9cfdde2cc7106b6f72ccbff2f0935737
Quyet24325 <quyet24325@gmail.com>
2024-11-14 12:24:16 +0700

    feat: category

--- view/admin/category/create.php
+++ view/admin/category/create.php
@@ -2,64 +2,63 @@
 <div class="page-body">
 
     <!-- New Product Add Start -->
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12">
-                <div class="row">
+    <form action="input.php?act=category_create" method="post" enctype="move_uploaded_file">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
 
-                    <div class="card">
-                        <div class="card-header-2">
-                            <h5>Thêm ảnh danh mục</h5>
-                        </div>
-                        <div class="card-body">
-                            <input type="file" name="image" id="" class="form-control">
-                        </div>
-                    </div>
-
-                    <div class="card">
-                        <div class="card-header-2">
-                            <h5>Thêm danh mục</h5>
-                        </div>
-
-                        <div class="theme-form theme-form-2 mega-form">
-                            <div class="mb-4 row align-items-center">
-                                <div class="col-sm-13">
-                                    <label class="form-label-title col-sm-3 mb-0">Tên danh mục</label>
-                                    <input class="form-control" type="text" name="name" placeholder="Tên danh mục">
-                                </div>
+                        <div class="card">
+                            <div class="card-header-2">
+                                <h5>Thêm ảnh danh mục</h5>
                             </div>
-
-                            <div class="mb-4 row align-items-center">
-                                <div class="col-sm-13">
-                                    <label class="form-label-title col-sm-2 mb-0">Tạng thái</label>
-                                    <select class="form-control" id="cater" name="status">
-                                        <option value="">Chọn trạng thái</option>
-                                        <option value="Hidden">Ẩn</option>
-                                        <option value="Active">Hiện</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="mb-6 row align-items-center">
-                                <div class="col-sm-13">
-                                    <label class="form-label-title col-sm-5 mb-3">Mô tả</label>
-                                    <textarea name="description" id="" class="form-control bg-light-subtle" placeholder="Mô tả...."></textarea>
-                                </div>
+                            <div class="card-body">
+                                <input type="file" name="image" id="" class="form-control">
                             </div>
                         </div>
-                        <br>
-                        <button type="submit" name="createCategory" class="btn btn-primary w-100">Thêm mới</button>
-                    </div>
-                    <div class="card">
-                            <div class="col-lg-2">
+
+                        <div class="card">
+                            <div class="card-header-2">
+                                <h5>Thêm danh mục</h5>
+                            </div>
+
+                            <div class="theme-form theme-form-2 mega-form">
+                                <div class="mb-4 row align-items-center">
+                                    <div class="col-sm-13">
+                                        <label class="form-label-title col-sm-3 mb-0">Tên danh mục</label>
+                                        <input class="form-control" type="text" name="name" placeholder="Tên danh mục">
+                                    </div>
+                                </div>
+
+                                <div class="mb-4 row align-items-center">
+                                    <div class="col-sm-13">
+                                        <label class="form-label-title col-sm-2 mb-0">Tạng thái</label>
+                                        <select class="form-control" id="cater" name="status">
+                                            <option value="">Chọn trạng thái</option>
+                                            <option value="Hidden">Ẩn</option>
+                                            <option value="Active">Hiện</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="mb-6 row align-items-center">
+                                    <div class="col-sm-13">
+                                        <label class="form-label-title col-sm-5 mb-3">Mô tả</label>
+                                        <textarea name="description" id="" class="form-control bg-light-subtle" placeholder="Mô tả...."></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <button type="submit" name="createCategory" class="btn btn-primary w-100">Thêm mới</button>
+                        </div>
+                        <div class="col-lg-2">
                             <a href="indext.php?act=category" class="btn btn-primary w-100">Quay lại</a>
                         </div>
-        
                     </div>
-
                 </div>
             </div>
         </div>
-    </div>
+    </form>
+
 </div>
 <!-- New Product Add End -->
 
